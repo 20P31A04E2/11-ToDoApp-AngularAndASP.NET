5c11c461142231d422697fc9a16d827acd10a45b
Karri-LakshmiSravanthi <karrilakshmisravanthi@gmail.com>
2024-07-25 11:35:52 +0530

    Made changes in hero section.

--- ToDoApp/ToDoList/dist/to-do-list/server/main.server.mjs
+++ ToDoApp/ToDoList/dist/to-do-list/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-AQETIXSB.mjs";import"./chunk-ZJUESG3Y.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-5V6BDHAM.mjs";import"./chunk-ZJUESG3Y.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
